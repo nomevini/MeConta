ddce5f7b22d46faf9456d61c369f1296457be192
Vinícius de Sousa Carvalho <sousav387@gmail.com>
2023-11-15 15:29:41 -0300

    feat:interface da tela de login concluída

--- frontend/pages/login/script.js
+++ frontend/pages/login/script.js
@@ -30,12 +30,36 @@ function validate() {
 
 // Função para mostrar o formulário de cadastro e ocultar o de login
 function showSignupForm() {
+  
   document.querySelector('.login-form').style.display = 'none';
   document.querySelector('.signup-form').style.display = 'flex';
+
+  let signupHeader = document.querySelector('.login-header');
+
+  // Verifica se o elemento foi encontrado
+  if (signupHeader) {
+      // Altera o conteúdo conforme necessário
+      signupHeader.innerHTML = `
+          <h1>Registre-se na plataforma</h1>
+          <p>Crie uma nova conta para começar a gerenciar suas finanças pessoais.</p>
+      `;
+  }
+
 }
 
 // Função para mostrar o formulário de login e ocultar o de cadastro
 function showLoginForm() {
   document.querySelector('.login-form').style.display = 'flex';
   document.querySelector('.signup-form').style.display = 'none';
+
+  let signupHeader = document.querySelector('.login-header');
+
+  // Verifica se o elemento foi encontrado
+  if (signupHeader) {
+      // Altera o conteúdo conforme necessário
+      signupHeader.innerHTML = `
+        <h1>Acesse a plataforma</h1>
+        <p>Faça login ou registre-se para começar a construir seus projetos ainda hoje.</p>  
+      `;
+  }
 }
