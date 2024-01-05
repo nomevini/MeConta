5a4fe9e54f1a61182ce20e6c8829b84653438f02
Vinícius de Sousa Carvalho <sousav387@gmail.com>
2024-01-05 17:05:23 -0300

    feat:rota para cadatro de transação

--- backend/src/controllers/transaction.js
+++ backend/src/controllers/transaction.js
@@ -1,26 +1,108 @@
+const User = require('../models/user')
+const MetodoPagamento = require('../models/paymentMethod')
+const Categoria = require('../models/transactionCategory')
+const Transacao = require('../models/transaction')
+
 const createTransaction = async (req, res) => {
     try {
-        const {descricao, categoria, metodoPagamento, valor, status, qtdParcelas, dataTransacao, metaId, usuarioId} = req.body
+        let {descricao, categoria, metodoPagamento, valor, status, qtdParcelas, dataTransacao, metaId, usuarioId} = req.body
         
         // verificar informacoes
         if (!descricao || !categoria || !metodoPagamento || !valor || !status || !qtdParcelas || !dataTransacao || !usuarioId) {
             return res.status(400).json({ error: 'Campos obrigatórios não fornecidos.'})  
         }
-
+        
         // verificar se o usuario existe
-
+        const user = await User.findByPk(usuarioId)
+        
+        if (!user) {
+            return res.status(404).json({message: 'Usuário não encontrado'})
+        }
 
         // verificar se o metodo de pagamento existe
+        const payment = await MetodoPagamento.findOne({
+            where: {
+                nome: metodoPagamento
+            }
+        })
 
+        if (!payment) {
+            return res.status(404).json({message: "Metodo de pagamento não cadastrado"})
+        }
 
+        // capturar id do metodo
+        const paymentId = payment.dataValues.id
+    
         // verificar se a categoria existe
+        const category = await Categoria.findOne({
+            where: {
+                nome: categoria
+            }
+        })
 
+        if (!category) {
+            return res.status(404).json({message: "Categoria não existente"})
+        }
 
-        // se a transacao for parcelada, dividir o valor em parcelas e inserir varias contas nos meses seguintes
-        // senão, inserir no mes atual
+        const categoryId = category.dataValues.id
 
-        return res.status(201).json({message:"Criado com sucesso"})
+        if (qtdParcelas > 1) {
+            // se a transacao for parcelada, dividir o valor em parcelas e inserir varias contas nos meses seguintes4
+
+            // Calcular o valor de cada parcela
+            const valorParcela = valor / qtdParcelas;
+
+            // Criar transações para cada parcela nos meses seguintes
+            const data = new Date(dataTransacao);
+            let counter = qtdParcelas
+
+            for (let i = 0; i < qtdParcelas; i++) {
+
+                await Transacao.create({
+                    descricao,
+                    categoria: categoryId,
+                    metodoPagamento: paymentId,
+                    valor: valorParcela,
+                    qtdParcelas: counter,
+                    dataTransacao: data,
+                    metaId,
+                    usuarioId,
+                    status,
+                });
+
+              
+                const mesAtual = data.getMonth()
+              
+                if (mesAtual == 11) {
+                    data.setMonth(0);
+                    data.setYear(data.getFullYear() + 1)
+                }else {
+                    data.setMonth(mesAtual + 1);
+                }
+                counter--;
+            }
+
+            return res.status(201).json({message: "transações criadas"})
+
+        }else {
+            // senão, inserir no mes atual
+            const transaction = await Transacao.create({
+                descricao,
+                categoria: categoryId,
+                metodoPagamento: paymentId,
+                valor,
+                qtdParcelas,
+                dataTransacao,
+                metaId,
+                usuarioId,
+                status
+            })
+
+            return res.status(201).json({message: 'transação cadastrada', transaction})
+        }
+
     } catch (error) {
+        console.log(error.message)
         return res.status(500).json({message: "Erro interno do servidor"})
     }
 }
