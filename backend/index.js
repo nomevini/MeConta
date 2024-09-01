cca88e98b51ae6f30e6190ee9c7523ced3040e2c
Vinícius de Sousa Carvalho <sousav387@gmail.com>
2024-09-01 17:01:30 -0300

    fix: cors update

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,7 @@ const routes = require('./src/routes/routes')
 const app = express()
 
 app.use(express.json())
-
-// Configuração do CORS para permitir requisições do localhost
-const corsOptions = {
-  origin: 'http://localhost:3000', // Substitua pelo número de porta correto se for diferente
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(routes)
 
 app.listen(3000)
